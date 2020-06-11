261740f391be3871aa981505c61be2be8108223a
Horst JENS <horstjens@gmail.com>
2020-06-11 17:13:50 +0200

    integer values from map and startpoints

--- battleship/game.py
+++ battleship/game.py
@@ -6,18 +6,32 @@ class Game:
     world = []  # list of list
     width = 0
     height = 0
+    fleets = []
+    startpoints = []
 
-    def __init__(self, mapfile="map001.csv"):
+    def __init__(self, mapfile="map001.csv", starpoints="map001_startpoints.csv"):
         with open(mapfile) as f:
             lines = f.readlines()
         for line in lines:
-            Game.world.append(line.split(",")[:-1])
+            textvalues = line.split(",")[:-1]
+            intvalues = [int(x) for x in textvalues]
+            Game.world.append(intvalues)
         Game.width = len(Game.world[0])
         Game.height = len(Game.world)
+
+        with open(starpoints) as f:
+            lines = f.readlines()
+        for line in lines:
+            x,y = line.split(",")
+            x = int(x)
+            y = int(y)
+            Game.startpoints.append((x,y))
 
         print("----- game world {} x {}".format(Game.width, Game.height))
         for line in Game.world:
             print(line)
+        print("-----startpoints----")
+        print(Game.startpoints)
 
 if __name__ == "__main__":
     g = Game()
